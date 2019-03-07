15165afde548197f9cbf5782cd27cfa75372c98b
Elvis Chagas <elvischagas123@gmail.com>
2019-03-07 17:28:37 -0300

    commit ternario

---
+++ fundamentos/ternario.js
@@ -0,0 +1,10 @@
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado"
+/* ou poderia colocar um corpo
+*       
+        return nota >= 7 ? "Aprovado" : "Reprovado"
+*
+*/
+
+console.log(resultado(6.8));
+console.log(resultado(7.5));
+
