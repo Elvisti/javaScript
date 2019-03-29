da7ac817b7d6fab299c7c41daf0116ef710765bc
Elvis Chagas <elvischagas123@gmail.com>
2019-03-29 17:47:12 -0300

    commit estrutura de controle

--- controle/if.js
+++ controle/if.js
@@ -1,9 +1,28 @@
 function soBoaNoticia(nota) {
     if (nota >= 7) {
-        console.log("Aprovado");
+        console.log("Aprovado com  " + nota);
         
     }else{
         console.log('Reprovado');
         
     }
 }soBoaNoticia(8)
+
+function seForVerdadeEuFalo(valor){
+    if (valor) {
+            console.log('É verdade... '+ valor);
+            
+    }
+}
+seForVerdadeEuFalo()
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(NaN)
+
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo("?")
+seForVerdadeEuFalo([3,2,2])
+seForVerdadeEuFalo({})
---
+++ controle/if2.js
@@ -0,0 +1,10 @@
+function teste1(num) {
+    if(num > 7)
+        console.log(num);
+        console.log('Final!!');
+        
+        
+}
+
+teste1(9)
+teste1(4)
