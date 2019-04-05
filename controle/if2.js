3f0fc605a45b9201cda33c2a29ba40a79b3fe70d
Elvis Chagas <elvischagas123@gmail.com>
2019-04-05 14:01:00 -0300

    commit if2

--- controle/if2.js
+++ controle/if2.js
@@ -1,10 +1,15 @@
 function teste1(num) {
     if(num > 7)
         console.log(num);
-        console.log('Final!!');
-        
-        
-}
 
-teste1(9)
-teste1(4)
+    console.log('Final!!');// Não está associada ao bloco        
+}
+//teste1(9)
+//teste1(4)
+
+function teste2(num){
+    if(num > 7); { // cuidado com o ;, não usar com as estruturas de controle.  
+        console.log(num);
+        
+    }
+}
