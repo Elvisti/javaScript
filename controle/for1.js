27067e63888f99439d66a2eab0b92dae5584adc9
Elvis Chagas <elvischagas123@gmail.com>
2019-04-12 18:19:13 -0300

    commit for1

---
+++ controle/for1.js
@@ -0,0 +1,18 @@
+let contador = 1
+
+while (contador <= 10) {
+    console.log(`Cantador ${contador}.`);
+    contador ++
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(`I ${i}`);
+    
+}
+ 
+const notas = [6.3, 4.9, 2.8, 7.9]
+
+for( let c = 0; c < notas.length; c++){
+    console.log(`Nota ${notas[c]}`);
+    
+}
