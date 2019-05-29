d459177b80eb8d8be61294b4f4420a5f817c832e
Elvis Chagas <elvischagas123@gmail.com>
2019-05-28 23:14:23 -0300

    commit ajax

---
+++ ajax/server.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser') // quando tiver uma requisição será interpretada pelo body-parse, deixando prontos para manipular os dados
+const express = require('express')
+const app = express()// instanciando o express
+
+app.use(express.static('.'))// Será executado o multer para um padrão de url ou algum especifico, no caso (.) na pasta atual
+app.use(bodyParser.urlencoded({extended: true}))// Se vir algum submite irá pegar os dados e transformar em objeto
+app.use(bodyParser.json())//Se vir algum formato json irá transformar em objeto
+
+
+app.get('/teste', (req, res) => res.send('OK'))// Quando houver uma requisição do tipo get na url /teste, irá retornar um OK
+app.listen(8080, () => console.log('Executando...'));// Iniciando o servidor na porta 8080
